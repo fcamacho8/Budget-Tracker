a7ced8d987c85dac39ab0a8ab5d407ff75c94b13
FCTESLA <fcamacho888@gmail.com>
2020-06-18 13:41:39 -0700

    created components

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron(props) {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Budget Tracker</h1>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;

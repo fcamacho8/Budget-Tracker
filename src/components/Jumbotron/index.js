3b8c4c06a0baca3050323e2d1ca693e15e4c2d5a
FCTESLA <fcamacho888@gmail.com>
2020-06-18 17:20:37 -0700

    handleInputChange

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -4,8 +4,7 @@ function Jumbotron(props) {
     return (
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
-                <h1 className="display-4">Budget Tracker</h1>
-                
+                <h1 className="display-4">Budget Tracker</h1> 
             </div>
         </div>
     )
